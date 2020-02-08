0c1768f04f853b87b3aaa3073714344696716d5c
Robin Stut <robin@voorhoede.nl>
2020-02-08 13:36:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => res.render("pages/index"));
+
+app.listen(3000);
