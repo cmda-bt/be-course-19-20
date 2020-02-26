eaf74cef79d27ccb3ba0216135555ede7d766c7f
Robin Stut <robin@voorhoede.nl>
2020-02-26 22:42:40 +0100

    feat(stripCityData.js): minify original city data

---
+++ static/stripCityData.js
@@ -0,0 +1,37 @@
+import originalCityData from '/originalCityData.js'
+
+// store unique items in this array
+let newArray = []
+
+// map over all originalCityData keys
+originalCityData.map((x, index) => {
+
+    // check if value is equal to eachother
+    const searchValue = newArrayItem => {
+        return newArrayItem.woonplaats === x.woonplaats;
+    }
+    
+    // if map is at last loop
+    if (index + 1 === originalCityData.length) {
+        console.log(stringify(newArray));
+    }
+
+    // if map doesn't exist in newArray
+    if (!newArray.find(searchValue)) {
+
+        // push unique item in newArray
+        newArray.push(x)
+        return
+    }
+    return
+})
+
+
+// stringify data
+function stringify(data) {
+    return JSON.stringify(data)
+}
+
+// result is stored in cleanedCityData.js
+// original data length = 4699
+// new data length = 2384
