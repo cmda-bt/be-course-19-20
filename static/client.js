544b886a945dfa8d13d3b3f71a25eb6a0c40bd7a
Robin Stut <robin@voorhoede.nl>
2020-03-23 09:43:26 +0100

    refactor(linting): running linting

--- static/client.js
+++ static/client.js
@@ -1,50 +1,50 @@
-import cityData from "./data/onlyCityData.js";
+import cityData from './data/onlyCityData.js'
 import {
   userLocation,
   stringify,
   upperCaseCorrection,
   removeFromDOM,
   addValueToInput,
-} from "./modules/helper.js";
+} from './modules/helper.js'
 
-const storeUserLocation = document.getElementById("userLocation");
-const submitUserLocationForm = document.getElementById("submitUserLocation");
-const customLocationTrigger = document.getElementById("getCustomLocation");
-const locationSuggestions = document.getElementById("locationSuggestions");
-const locationFormInputs = document.getElementById("locationInputTypes");
+const storeUserLocation = document.getElementById('userLocation')
+const submitUserLocationForm = document.getElementById('submitUserLocation')
+const customLocationTrigger = document.getElementById('getCustomLocation')
+const locationSuggestions = document.getElementById('locationSuggestions')
+const locationFormInputs = document.getElementById('locationInputTypes')
 
 function setGeoLocationButtonInForm() {
   locationFormInputs.insertAdjacentHTML(
-    "afterbegin",
+    'afterbegin',
     `
        <    utton id="getUserLocationByGeoAPI"></button> 
-    `
-  );
+    `,
+  )
 
   document
-    .getElementById("getUserLocationByGeoAPI")
-    .addEventListener("click", getLocationTrigger);
+    .getElementById('getUserLocationByGeoAPI')
+    .addEventListener('click', getLocationTrigger)
 }
-setGeoLocationButtonInForm();
+setGeoLocationButtonInForm()
 
 // listen to button click
-customLocationTrigger.addEventListener("keydown", getCustomLocation);
+customLocationTrigger.addEventListener('keydown', getCustomLocation)
 
 // gets triggered when custom location input is getting filled
 function getCustomLocation(e) {
   // converts user input to uppercase letter words
-  const correctedInputValue = upperCaseCorrection(e.target.value + e.key);
-  const preventKeys = ["ArrowDown", "Tab"];
+  const correctedInputValue = upperCaseCorrection(e.target.value + e.key)
+  const preventKeys = ['ArrowDown', 'Tab']
 
   // if inputvalue is longer than 2 and last typed key is not a key in preventKeys
   if (correctedInputValue.length > 1 && !preventKeys.includes(e.key)) {
     // filter all results of json data with the typed value
     const filteredData = cityData.filter((item) =>
-      item.includes(correctedInputValue)
-    );
+      item.includes(correctedInputValue),
+    )
 
     // removes previous suggestions
-    removeFromDOM(locationSuggestions);
+    removeFromDOM(locationSuggestions)
 
     // loop over filtered result
     filteredData.map((x, index) => {
@@ -52,75 +52,75 @@ function getCustomLocation(e) {
       if (index < 5) {
         // renders suggestions
         locationSuggestions.insertAdjacentHTML(
-          "beforeend",
+          'beforeend',
           `
                     <input type="radio" id="${x}" name="userSuggestion" value="${x}" ${
-            index === 0 ? 'tabindex="0"' : ""
+            index === 0 ? 'tabindex="0"' : ''
           }>   
-                    <label for="${x}">${x}</label>`
-        );
+                    <label for="${x}">${x}</label>`,
+        )
       }
-    });
+    })
 
     // converts classes a.k.a HTMLCollection to mappable array
-    const HTMLCollectionToArray = Array.from(locationSuggestions);
+    const HTMLCollectionToArray = Array.from(locationSuggestions)
 
     // loop over classes
     HTMLCollectionToArray.map((x) => {
       // create eventListener for buttons
-      x.addEventListener("click", suggestedLocationTrigger);
-    });
+      x.addEventListener('click', suggestedLocationTrigger)
+    })
   } else if (preventKeys.includes(e.key)) {
-    console.log("preventkey " + e.key);
+    console.log('preventkey ' + e.key)
     const suggestionInput = document.getElementsByClassName(
-      "suggestionInput"
-    )[0];
+      'suggestionInput',
+    )[0]
     // const suggestionInput = document.getElementById('noLocation')
-    console.log(suggestionInput);
-    suggestionInput.focus();
+    console.log(suggestionInput)
+    suggestionInput.focus()
   }
 }
 
 // gets triggered when suggested location is clicked
 function suggestedLocationTrigger(e) {
   // get location from event parameter
-  const location = e.target.dataset.location;
+  const location = e.target.dataset.location
 
   // gives feedback to user, shows what is clicked in input
-  addValueToInput(customLocationTrigger, e.target.innerText);
+  addValueToInput(customLocationTrigger, e.target.innerText)
 
   // removes suggestions
-  removeFromDOM(locationSuggestions);
+  removeFromDOM(locationSuggestions)
 
   // stores location in hidden input
-  storeLocation(location);
+  storeLocation(location)
 }
 
 // store location in hidden input
 function storeLocation(location) {
   // store data in input hidden field
-  addValueToInput(storeUserLocation, stringify(location));
+  addValueToInput(storeUserLocation, stringify(location))
 
-  console.log("API data is added");
+  console.log('API data is added')
 }
 
 // gets triggered when Get Location button is clicked
 async function getLocationTrigger(e) {
   //prevents form from submitting
-  e.preventDefault();
+  e.preventDefault()
 
-  submitUserLocationForm.disabled = true;
+  submitUserLocationForm.disabled = true
 
   // fetch Geo Location
   const getLocation = await userLocation().then((x) => {
     // enable submit button
-    submitUserLocationForm.disabled = false;
+    submitUserLocationForm.disabled = false
     document
-      .getElementById("getUserLocationByGeoAPI")
-      .setAttribute("class", "clicked");
-    return x;
-  });
+      .getElementById('getUserLocationByGeoAPI')
+      .setAttribute('class', 'clicked')
+    return x
+  })
 
   // stores location in hidden input
-  storeLocation(getLocation);
+  storeLocation(getLocation)
 }
--- static/modules/helper.js
+++ static/modules/helper.js
@@ -1,42 +1,49 @@
 // get user location from GEO API
 async function userLocation() {
+  // waits for GEO location api to resolve promise
+  const data = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
 
-    // waits for GEO location api to resolve promise
-    const data = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-
-    return { latitude: data.coords.latitude, longitude: data.coords.longitude }
+  return { latitude: data.coords.latitude, longitude: data.coords.longitude }
 }
 
 // converts data to JSON
 function stringify(data) {
-    return JSON.stringify(data)
+  return JSON.stringify(data)
 }
 
-// converts JSON to data 
+// converts JSON to data
 function parse(json) {
-    return JSON.parse(json)
+  return JSON.parse(json)
 }
 
 // converts input to Uppercase first word letters
 function upperCaseCorrection(input) {
-    // source: https://stackoverflow.com/a/4878800
-    const newInput = input.toLowerCase()
-        .split(' ')
-        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-        .join(' ');
+  // source: https://stackoverflow.com/a/4878800
+  const newInput = input
+    .toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ')
 
-    return newInput
+  return newInput
 }
 
 // removes suggestions
 function removeFromDOM(DOMElement) {
-    DOMElement.innerHTML = ""
+  DOMElement.innerHTML = ''
 }
 
 function addValueToInput(DOMElement, newValue) {
-    DOMElement.value = newValue   
+  DOMElement.value = newValue
 }
 
-export { userLocation, stringify, parse, upperCaseCorrection, removeFromDOM, addValueToInput };
+export {
+  userLocation,
+  stringify,
+  parse,
+  upperCaseCorrection,
+  removeFromDOM,
+  addValueToInput,
+}
